bcb77b6f437d9901368a8951cbdbb9a845d9f807
Jonathan Kila <temidee11@yahoo.co.uk>
2023-04-22 12:08:00 +0100

    server down apr 2023

--- src/components/Demo/Demo.jsx
+++ src/components/Demo/Demo.jsx
@@ -1,7 +1,8 @@
 const Demo = () => (
   <div className="demo">
-    This application is for demo purposes only. Please use mock data. Please try
-    it out by making an account.
+    {/* This application is for demo purposes only. Please use mock data. Please try
+    it out by making an account. */}
+    The server is currently down. You will not be able to login or register.
   </div>
 )
 
--- src/store/main-context.js
+++ src/store/main-context.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, memo } from "react"
+import React, { useState, useEffect, useContext } from "react"
 import { useNavigate } from "react-router-dom"
 import AuthContext from "./auth-context"
 import {
